ff88434c4231868172b399127313f01a552808ba
Pedro Felipe Gomez Bonilla <pipe22007@Pedros-MacBook-Pro.local>
2021-08-06 09:02:28 -0500

    Adding Instructions

--- src/edu/upb/algoritmos/lab1/Main.java
+++ src/edu/upb/algoritmos/lab1/Main.java
@@ -1,9 +1,13 @@
 package edu.upb.algoritmos.lab1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner sc = new Scanner(System.in);
+
+
     }
 }
 //Developed by Pedro Felipe Gómez Bonilla - ID:000396221
