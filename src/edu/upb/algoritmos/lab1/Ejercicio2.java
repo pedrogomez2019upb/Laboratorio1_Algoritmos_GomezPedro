95ad0bb4d9cfbd0b05b8523ebad5c3e3b88d680f
Pedro Felipe Gomez Bonilla <pipe22007@Pedros-MacBook-Pro.local>
2021-08-06 11:26:54 -0500

    finishing exercise 2

--- src/edu/upb/algoritmos/lab1/Ejercicio2.java
+++ src/edu/upb/algoritmos/lab1/Ejercicio2.java
@@ -1,7 +1,44 @@
 package edu.upb.algoritmos.lab1;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    //We create two functions , one for a homemade gauss formula and also a difference algorithm for the same use
+    public static int diyGauss(int n){
+        int gauss=(n*(n+1))/2;
+        return gauss;
+    }
+    public static int differenceGauss(int houses,int counter){
+        int difference=(((houses*(houses+1))/2)-counter);
+        return difference;
+    }
+    public static void main(String[] args)
+    {
+        //We create a int variable so that we can store the result according to the conditions made in the exercise.
+        int result=-1;
+        Scanner sc = new Scanner(System.in);
+        //We ask the houses so that we can calculate the condition.
+        System.out.println("\nHi! Welcome to the Street Houses program!");
+        System.out.println("\nPlease, enter the number of houses that there are in the street:");
+        int houses = sc.nextInt();
+        for(int i=1;i<=houses;i++){
+            //We start with n always being i-1 , because everytime a house passes we need to substract 1
+            int n=(i-1);
+            //We get the two values
+            int gauss=diyGauss(n);
+            int difGauss=differenceGauss(houses,i);
+            //If the difference is the same result as Gauss , we return the number of houses, if not we return -1
+            if(difGauss-gauss==gauss){
+                result=i;
+                break;
+            }
+        }
+        System.out.println("\nThe number of houses for "+houses+" houses is:");
+        System.out.println(result);
+        System.out.println("\nIf the answer is '-1' it means that it doesn't satisfy the condition.");
+        System.out.println("\nThanks for using the Street Houses Program!");
+
+
     }
 }
+//Developed by Pedro Felipe Gómez Bonilla - ID:000396221
